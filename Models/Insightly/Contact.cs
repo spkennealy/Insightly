a7a18f4379a3eb913a7fcccebfaf11b4d3f1eda9
Sean Kennealy <sean@insight.ly>
2020-09-15 17:23:32 -0700

    Models- Add InsightlyObject.cs

--- Models/Insightly/Contact.cs
+++ Models/Insightly/Contact.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Models.Insightly
 {
---
+++ Models/Insightly/InsightlyObject.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Models.Insightly
+{
+    public class InsightlyObject
+    {
+        [JsonProperty("CUSTOMFIELDS", NullValueHandling = NullValueHandling.Ignore)]
+        public List<CustomField> CUSTOMFIELDS { get; set; }
+    }
+}
