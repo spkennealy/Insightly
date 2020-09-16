2fb6564d000bb666d119c6383720a6ac8e1d9f25
Sean Kennealy <spkennealy@gmail.com>
2020-09-15 17:29:05 -0700

    Models- Add Tag.cs

--- Models/Insightly/InsightlyCustomFieldObject.cs
+++ Models/Insightly/InsightlyCustomFieldObject.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 
-namespace Models
+namespace Models.Insightly
 {
     public class InsightlyCustomFieldObject
     {
---
+++ Models/Insightly/Tag.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class Tag
+    {
+        [JsonProperty("TAG_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string TAG_NAME { get; set; }
+    }
+}
