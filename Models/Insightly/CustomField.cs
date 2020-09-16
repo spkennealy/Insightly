7b276bc4c2d173c490c2785bf1a658e6e9636da9
Sean Kennealy <spkennealy@gmail.com>
2020-09-15 17:30:25 -0700

    Models- Add CustomField.cs

---
+++ Models/Insightly/CustomField.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class CustomField
+    {
+        [JsonProperty("FIELD_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string FIELD_NAME { get; set; }
+
+        [JsonProperty("FIELD_VALUE", NullValueHandling = NullValueHandling.Ignore)]
+        public object FIELD_VALUE { get; set; }
+
+        [JsonProperty("CUSTOM_FIELD_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public string CUSTOM_FIELD_ID { get; set; }
+    }
+}
