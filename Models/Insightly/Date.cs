a7e9f3014013b0c9c755ad7084622e60e1e0bcda
Sean Kennealy <sean@insight.ly>
2020-09-17 19:05:00 -0700

    Models- Insightly.Date added

---
+++ Models/Insightly/Date.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Models.Insightly
+{
+    public class Date
+    {
+        [JsonProperty("DATE_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? DATE_ID { get; set; }
+
+        [JsonProperty("OCCASION_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string OCCASION_NAME { get; set; }
+
+        [JsonProperty("OCCASION_DATE", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? OCCASION_DATE { get; set; }
+
+        [JsonProperty("REPEAT_YEARLY", NullValueHandling = NullValueHandling.Ignore)]
+        public bool REPEAT_YEARLY { get; set; }
+
+        [JsonProperty("CREATE_TASK_YEARLY", NullValueHandling = NullValueHandling.Ignore)]
+        public bool CREATE_TASK_YEARLY { get; set; }
+    }
+}
