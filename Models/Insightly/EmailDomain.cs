9c2c2e7f6d715a7b470340146237f793c7eff13d
Sean Kennealy <spkennealy@gmail.com>
2020-09-21 17:28:53 -0700

    Models.Insightly.EmailDomain added

---
+++ Models/Insightly/EmailDomain.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class EmailDomain
+    {
+        [JsonProperty("EMAIL_DOMAIN_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? EMAIL_DOMAIN_ID { get; set; }
+
+        [JsonProperty("EMAIL_DOMAIN", NullValueHandling = NullValueHandling.Ignore)]
+        public string EMAIL_DOMAIN { get; set; }
+    }
+}
