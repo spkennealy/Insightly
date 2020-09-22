dfcfb009ae58052366c0a0a4db0cbdf4d1f9e07d
Sean Kennealy <spkennealy@gmail.com>
2020-09-21 17:31:22 -0700

    Models.Insightly.InsightlyRecordWrapper added

---
+++ Models/Insightly/InsightlyRecordWrapper.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class InsightlyRecordWrapper
+    {
+        [JsonProperty("recordId")]
+        public long? RECORD_ID { get; set; }
+
+        [JsonProperty("recordType")]
+        public string RECORD_TYPE { get; set; }
+
+        [JsonProperty("operation")]
+        public string OPERATION { get; set; }
+
+        [JsonProperty("newValue")]
+        public object NEW_VALUE { get; set; }
+
+        [JsonProperty("oldEntity", NullValueHandling = NullValueHandling.Ignore)]
+        public object OLD_VALUE { get; set; }
+    }
+}
