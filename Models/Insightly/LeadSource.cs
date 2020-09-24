989113d989a5a8447271c0a0e1c5043d5ae78319
Sean Kennealy <spkennealy@gmail.com>
2020-09-23 18:22:33 -0700

    Models.Insightly.LeadSource added

---
+++ Models/Insightly/LeadSource.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class LeadSource
+    {
+        [JsonProperty("LEAD_SOURCE_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? LEAD_SOURCE_ID { get; set; }
+
+        [JsonProperty("LEAD_SOURCE", NullValueHandling = NullValueHandling.Ignore)]
+        public string LEAD_SOURCE { get; set; }
+
+        [JsonProperty("DEFAULT_VALUE", NullValueHandling = NullValueHandling.Ignore)]
+        public bool DEFAULT_VALUE { get; set; }
+
+        [JsonProperty("FIELD_ORDER", NullValueHandling = NullValueHandling.Ignore)]
+        public long? FIELD_ORDER { get; set; }
+    }
+}
