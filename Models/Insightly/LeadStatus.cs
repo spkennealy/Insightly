479f14d7d78db384694228d43e870b7313fc552e
Sean Kennealy <spkennealy@gmail.com>
2020-09-23 18:23:32 -0700

    Models.Insightly.LeadStatus added

---
+++ Models/Insightly/LeadStatus.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class LeadStatus
+    {
+        [JsonProperty("LEAD_STATUS_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? LEAD_STATUS_ID { get; set; }
+
+        [JsonProperty("LEAD_STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public string LEAD_STATUS { get; set; }
+
+        [JsonProperty("DEFAULT_STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public bool DEFAULT_STATUS { get; set; }
+
+        [JsonProperty("STATUS_TYPE", NullValueHandling = NullValueHandling.Ignore)]
+        public long? STATUS_TYPE { get; set; }
+
+        [JsonProperty("FIELD_ORDER", NullValueHandling = NullValueHandling.Ignore)]
+        public long? FIELD_ORDER { get; set; }
+    }
+}
