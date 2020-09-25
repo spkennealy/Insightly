4a2605d9ab98b01ea14d4be9fb310ae950ea9c1e
Sean Kennealy <spkennealy@gmail.com>
2020-09-24 19:33:33 -0700

    Insightly.Models.Link added

---
+++ Models/Insightly/Link.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class Link
+    {
+        [JsonProperty("RELATIONSHIP_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? RELATIONSHIP_ID { get; set; }
+
+        [JsonProperty("IS_FORWARD", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IS_FORWARD { get; set; }
+
+        [JsonProperty("DETAILS", NullValueHandling = NullValueHandling.Ignore)]
+        public string DETAILS { get; set; }
+
+        [JsonProperty("ROLE", NullValueHandling = NullValueHandling.Ignore)]
+        public string ROLE { get; set; }
+
+        [JsonProperty("LINK_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? LINK_ID { get; set; }
+
+        [JsonProperty("OBJECT_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string OBJECT_NAME { get; set; }
+
+        [JsonProperty("OBJECT_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? OBJECT_ID { get; set; }
+
+        [JsonProperty("LINK_OBJECT_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string LINK_OBJECT_NAME { get; set; }
+
+        [JsonProperty("LINK_OBJECT_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? LINK_OBJECT_ID { get; set; }
+
+    }
+}
