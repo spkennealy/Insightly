da40ba0391de8bc716e1ac304395530b8329cf77
Sean Kennealy <spkennealy@gmail.com>
2020-09-25 07:50:57 -0700

    Models.Insighlty.Note added

---
+++ Models/Insightly/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class Note
+    {
+        [JsonProperty("NOTE_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NOTE_ID { get; set; }
+
+        [JsonProperty("TITLE", NullValueHandling = NullValueHandling.Ignore)]
+        public string TITLE { get; set; }
+
+        [JsonProperty("BODY", NullValueHandling = NullValueHandling.Ignore)]
+        public string BODY { get; set; }
+
+        [JsonProperty("DATE_CREATED_UTC", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? DATE_CREATED_UTC { get; set; }
+
+        [JsonProperty("DATE_UPDATED_UTC", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? DATE_UPDATED_UTC { get; set; }
+    }
+}
