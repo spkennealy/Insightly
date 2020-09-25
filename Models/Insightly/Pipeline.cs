95c5bd739f8efe81102640918fc3a436ee4d9e17
Sean Kennealy <spkennealy@gmail.com>
2020-09-25 07:58:05 -0700

    Models.Insightly.Pipeline added

---
+++ Models/Insightly/Pipeline.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly
+{
+    public class Pipeline
+    {
+        [JsonProperty("PIPELINE_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? PIPELINE_ID { get; set; }
+
+        [JsonProperty("PIPELINE_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string PIPELINE_NAME { get; set; }
+
+        [JsonProperty("FOR_OPPORTUNITIES", NullValueHandling = NullValueHandling.Ignore)]
+        public bool FOR_OPPORTUNITIES { get; set; }
+
+        [JsonProperty("FOR_PROJECTS", NullValueHandling = NullValueHandling.Ignore)]
+        public bool FOR_PROJECTS { get; set; }
+
+        [JsonProperty("OWNER_USER_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? OWNER_USER_ID { get; set; }
+
+        [JsonProperty("PIPELINE_STAGE_CHANGE", NullValueHandling = NullValueHandling.Ignore)]
+        public PipelineStageChange PIPELINE_STAGE_CHANGE { get; set; }
+    }
+}
