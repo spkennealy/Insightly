337aa271b8b5ffd2f06f3d4debebbe664bdf4b25
Sean Kennealy <spkennealy@gmail.com>
2020-10-05 21:54:15 -0700

    Models.Insightly.CustomFieldDetails.CustomFieldOption added

---
+++ Models/Insightly/CustomFieldDetails/CustomFieldOption.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Insightly.CustomFieldDetails
+{
+    public class CustomFieldOption
+    {
+        [JsonProperty("OPTION_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? OPTION_ID { get; set; }
+
+        [JsonProperty("OPTION_VALUE", NullValueHandling = NullValueHandling.Ignore)]
+        public string OPTION_VALUE { get; set; }
+
+        [JsonProperty("OPTION_DEFAULT", NullValueHandling = NullValueHandling.Ignore)]
+        public bool OPTION_DEFAULT { get; set; }
+    }
+}
