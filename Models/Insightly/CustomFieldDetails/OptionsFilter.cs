574dff51897965a355d541d50de4fca1a4da4f4c
Sean Kennealy <spkennealy@gmail.com>
2020-10-05 21:56:02 -0700

    Models.Insightly.CustomFieldDetails.OptionsFilter added

---
+++ Models/Insightly/CustomFieldDetails/OptionsFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Insightly.CustomFieldDetails
+{
+    public class OptionsFilter
+    {
+        [JsonProperty("CONTROLLING_VALUE", NullValueHandling = NullValueHandling.Ignore)]
+        public string CONTROLLING_VALUE { get; set; }
+
+        [JsonProperty("OPTION_IDS", NullValueHandling = NullValueHandling.Ignore)]
+        public List<long?> OPTION_IDS { get; set; }
+    }
+}
