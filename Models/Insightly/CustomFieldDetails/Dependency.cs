b371e0acb472d58f988e3b7ba4117440311a95f7
Sean Kennealy <spkennealy@gmail.com>
2020-10-05 21:55:15 -0700

    Models.Insightly.CustomFieldDetails.Dependency added

---
+++ Models/Insightly/CustomFieldDetails/Dependency.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Insightly.CustomFieldDetails
+{
+    public class Dependency
+    {
+        [JsonProperty("CONTROLLING_FIELD_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public string CONTROLLING_FIELD_ID { get; set; }
+
+        [JsonProperty("OPTIONS_FILTERS", NullValueHandling = NullValueHandling.Ignore)]
+        public List<OptionsFilter> OPTIONS_FILTERS { get; set; }
+    }
+}
