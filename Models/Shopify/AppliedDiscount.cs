5188a31c25ec2fc0fd1b91d5413ee914cea5a19b
Sean Kennealy <spkennealy@gmail.com>
2020-10-07 08:32:41 -0700

    Models.Shopify.AppliedDiscount added

---
+++ Models/Shopify/AppliedDiscount.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class AppliedDiscount
+    {
+        [JsonProperty("title")]
+        public string TITLE { get; set; }
+
+        [JsonProperty("description")]
+        public string DESCRIPTION { get; set; }
+
+        [JsonProperty("value")]
+        public string VALUE { get; set; }
+
+        [JsonProperty("value_type")]
+        public string VALUE_TYPE { get; set; }
+
+        [JsonProperty("amount")]
+        public string AMOUNT { get; set; }
+    }
+}
