35e818509bdd6eb534ae2c67dbac9359403c9085
Sean Kennealy <spkennealy@gmail.com>
2020-10-07 08:29:35 -0700

    Renamed Services project and moved ApiClient into Insightly folder

--- InsightlyApiServices/Insightly/ApiClient.cs
+++ InsightlyApiServices/Insightly/ApiClient.cs
@@ -7,7 +7,7 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace InsightlyApiServices
+namespace Services.Insightly
 {
     public class ApiClient
     {
