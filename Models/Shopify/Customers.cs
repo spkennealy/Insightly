f2e311dc813e29850e3cfcf202c20931971298a2
Sean Kennealy <spkennealy@gmail.com>
2020-10-08 09:16:22 -0700

    Models.Shopify.Customers added

---
+++ Models/Shopify/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class CustomersWrapper
+    {
+        [JsonProperty("customers")]
+        public List<Customer> CUSTOMERS { get; set; }
+
+        [JsonProperty("customer")]
+        public Customer CUSTOMER { get; set; }
+    }
+}
