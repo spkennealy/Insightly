0e7a942c06ca3e37a42adcd79496ea0bd2196a5b
Sean Kennealy <spkennealy@gmail.com>
2020-10-09 07:50:35 -0700

    Models.Shopify.DraftOrderWebhook added

---
+++ Models/Shopify/DraftOrderWebhook.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class DraftOrderWebhook
+    {
+        [JsonProperty("id")]
+        public long? ID { get; set; }
+
+        [JsonProperty("order_id")]
+        public long? ORDER_ID { get; set; }
+    }
+}
