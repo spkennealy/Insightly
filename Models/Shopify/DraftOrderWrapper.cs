14ed02ba37b7021c2ceb8bfed75da29559d01e5d
Sean Kennealy <spkennealy@gmail.com>
2020-10-09 07:51:47 -0700

    Models.Shopify.DraftOrderWrapper added

---
+++ Models/Shopify/DraftOrderWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class DraftOrderWrapper
+    {
+        [JsonProperty("draft_order")]
+        public DraftOrder DRAFT_ORDER { get; set; }
+
+        [JsonProperty("draft_orders")]
+        public List<DraftOrder> DRAFT_ORDERS { get; set; }
+
+        [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
+        public string ERROR { get; set; }
+    }
+}
