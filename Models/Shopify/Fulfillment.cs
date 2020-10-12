2df94b5c453186ea4dbb2aa268d86f16d88bd24f
Sean Kennealy <spkennealy@gmail.com>
2020-10-12 08:47:33 -0700

    Models.Shopify.LineItem added

--- Models/Shopify/Fulfillment.cs
+++ Models/Shopify/Fulfillment.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Models.Shopify
 {
     public class Fulfillment
     {
-        public Fulfillment()
-        {
-        }
+        [JsonProperty("created_at")]
+        public DateTime? CREATED_AT { get; set; }
+
+        [JsonProperty("id")]
+        public long? ID { get; set; }
+
+        [JsonProperty("order_id")]
+        public long? ORDER_ID { get; set; }
+
+        [JsonProperty("status")]
+        public string STATUS { get; set; }
+
+        [JsonProperty("tracking_company")]
+        public string TRACKING_COMPANY { get; set; }
+
+        [JsonProperty("tracking_number")]
+        public string TRACKING_NUMBER { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime? UPDATED_AT { get; set; }
     }
 }
---
+++ Models/Shopify/LineItem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class LineItem
+    {
+        [JsonProperty("custom")]
+        public bool CUSTOM { get; set; }
+
+        [JsonProperty("fulfillable_quantity")]
+        public int? FULFILLABLE_QUANTITY { get; set; }
+
+        [JsonProperty("fulfillment_service")]
+        public string FULFILLMENT_SERVICE { get; set; }
+
+        [JsonProperty("grams")]
+        public int? GRAMS { get; set; }
+
+        [JsonProperty("id")]
+        public long? ID { get; set; }
+
+        [JsonProperty("price")]
+        public string PRICE { get; set; }
+
+        [JsonProperty("product_id")]
+        public long? PRODUCT_ID { get; set; }
+
+        [JsonProperty("quantity")]
+        public int QUANTITY { get; set; }
+
+        [JsonProperty("requires_shipping")]
+        public bool REQUIRES_SHIPPING { get; set; }
+
+        [JsonProperty("sku")]
+        public string SKU { get; set; }
+
+        [JsonProperty("title")]
+        public string TITLE { get; set; }
+
+        [JsonProperty("variant_id")]
+        public long? VARIANT_ID { get; set; }
+
+        [JsonProperty("variant_title")]
+        public string VARIANT_TITLE { get; set; }
+
+        [JsonProperty("vendor")]
+        public string VENDOR { get; set; }
+
+        [JsonProperty("name")]
+        public string NAME { get; set; }
+
+        [JsonProperty("gift_card")]
+        public bool GIFT_CARD { get; set; }
+
+        [JsonProperty("properties")]
+        public List<Obj> PROPERTIES { get; set; }
+
+        [JsonProperty("taxable")]
+        public bool TAXABLE { get; set; }
+
+        [JsonProperty("tax_lines")]
+        public List<TaxLine> TAX_LINES { get; set; }
+
+        [JsonProperty("applied_discount")]
+        public AppliedDiscount APPLIED_DISCOUNT { get; set; }
+    }
+}
