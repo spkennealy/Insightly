b87264a15a0f53a884afb398928a63d8fe979015
Sean Kennealy <spkennealy@gmail.com>
2020-10-13 09:04:08 -0700

    Models.Shopify.Obj added

---
+++ Models/Shopify/Obj.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class Obj
+    {
+        [JsonProperty("name")]
+        public string NAME { get; set; }
+
+        [JsonProperty("value")]
+        public string VALUE { get; set; }
+    }
+}
