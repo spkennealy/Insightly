c2124eabe3cc096de3e39ce27a65750d2aabc9a3
Sean Kennealy <spkennealy@gmail.com>
2020-10-13 09:03:27 -0700

    Models.Shopify.MetafieldWrapper added

---
+++ Models/Shopify/MetafieldWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class MetafieldWrapper
+    {
+        [JsonProperty("metafield")]
+        public Metafield METAFIELD { get; set; }
+
+        [JsonProperty("metafields")]
+        public List<Metafield> METAFIELDS { get; set; }
+    }
+}
