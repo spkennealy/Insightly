df02deff1633d489a5723c6a1401fc2abec8139d
Sean Kennealy <spkennealy@gmail.com>
2020-10-15 08:54:11 -0700

    Models.Shopify.TempFulfillment added

---
+++ Models/Shopify/TempFulfillment.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class TempFulfillment
+    {
+        [JsonProperty("fulfillment_id")]
+        public long? FULFILLMENT_ID { get; set; }
+
+        [JsonProperty("order_id")]
+        public long? ORDER_ID { get; set; }
+    }
+}
