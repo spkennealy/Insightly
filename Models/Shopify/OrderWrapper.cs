a197b1e52fa8528d83234a0724b9e8acac137ae7
Sean Kennealy <spkennealy@gmail.com>
2020-10-15 08:36:23 -0700

    Models.Shopify.OrderWrapper added

---
+++ Models/Shopify/OrderWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class OrderWrapper
+    {
+        [JsonProperty("orders")]
+        public List<Order> ORDERS { get; set; }
+
+        [JsonProperty("order")]
+        public Order ORDER { get; set; }
+    }
+}
