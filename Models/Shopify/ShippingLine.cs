81287f6786001e5c85c99048f4e52e6b03e79a6c
Sean Kennealy <spkennealy@gmail.com>
2020-10-15 08:52:30 -0700

    Models.Shopify.ShippingLine added

---
+++ Models/Shopify/ShippingLine.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class ShippingLine
+    {
+        [JsonProperty("id")]
+        public long? ID { get; set; }
+
+        [JsonProperty("handle")]
+        public string HANDLE { get; set; }
+
+        [JsonProperty("price")]
+        public double? PRICE { get; set; }
+
+        [JsonProperty("title")]
+        public string TITLE { get; set; }
+
+        [JsonProperty("custom")]
+        public bool CUSTOM { get; set; }
+    }
+}
