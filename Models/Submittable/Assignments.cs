4417fe76bb01d4697792b25d2cbe3e2f8a49269b
Sean Kennealy <spkennealy@gmail.com>
2020-10-20 09:06:05 -0700

    Models.Submittable.Assignments added

---
+++ Models/Submittable/Assignments.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Assignments
+    {
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string TYPE { get; set; }
+
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string URL { get; set; }
+
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public int? COUNT { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Assignment> ITEMS { get; set; }
+    }
+}
