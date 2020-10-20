8d0ea2742c71dfc6466684ea9ea7f89e2206d7fb
Sean Kennealy <spkennealy@gmail.com>
2020-10-19 19:56:48 -0700

    Models.Shopify.Variant added

---
+++ Models/Shopify/Variant.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Shopify
+{
+    public class Variant
+    {
+        [JsonProperty("id")]
+        public long? ID { get; set; }
+
+        [JsonProperty("product_id")]
+        public long? PRODUCT_ID { get; set; }
+
+        [JsonProperty("sku")]
+        public string SKU { get; set; }
+    }
+}
