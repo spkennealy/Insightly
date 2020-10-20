161b834a8d0c30d5de9353b0f9626820fd6012cf
Sean Kennealy <spkennealy@gmail.com>
2020-10-20 08:29:22 -0700

    Models.ExchangeRatesApi.ExchangeRates added

---
+++ Models/ExchangeRatesApi/ExchangeRates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.ExchangeRatesApi
+{
+    public class ExchangeRates
+    {
+        public Dictionary<string, decimal> RATES { get; set; }
+
+        public string BASE { get; set; }
+
+        public DateTime? DATE { get; set; }
+    }
+}
