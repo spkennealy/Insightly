bbcfa35edacb910e7626bbdbae0a342bc6a62d70
Sean Kennealy <spkennealy@gmail.com>
2020-10-20 09:05:30 -0700

    Models.Submittable.Assignment added

---
+++ Models/Submittable/Assignment.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Assignment
+    {
+        [JsonProperty("user_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string USER_ID { get; set; }
+
+        [JsonProperty("staff_name", NullValueHandling = NullValueHandling.Ignore)]
+        public string STAFF_NAME { get; set; }
+
+        [JsonProperty("permission_level", NullValueHandling = NullValueHandling.Ignore)]
+        public string PERMISSION_LEVEL { get; set; }
+
+        [JsonProperty("permission_value", NullValueHandling = NullValueHandling.Ignore)]
+        public int? PERMISSION_VALUE { get; set; }
+    }
+}
