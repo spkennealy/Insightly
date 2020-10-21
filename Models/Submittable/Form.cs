b09f1daf52f802f971f3c9e2699db15eb1a744b1
Sean Kennealy <spkennealy@gmail.com>
2020-10-21 16:57:07 -0700

    Models.Submittable.Form added

---
+++ Models/Submittable/Form.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Form
+    {
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string TYPE { get; set; }
+
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string URL { get; set; }
+
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public int? COUNT { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<FormField> ITEMS { get; set; }
+    }
+}
