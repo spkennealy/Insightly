28ca5c52096aa7467624d3518a4dc45210e4c44f
Sean Kennealy <spkennealy@gmail.com>
2020-10-21 16:58:25 -0700

    Models.Submittable.Label added

---
+++ Models/Submittable/Label.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Label
+    {
+        [JsonProperty("label_text", NullValueHandling = NullValueHandling.Ignore)]
+        public string LABEL_TEXT { get; set; }
+
+        [JsonProperty("label_color1", NullValueHandling = NullValueHandling.Ignore)]
+        public string LABEL_COLOR_1 { get; set; }
+
+        [JsonProperty("label_color2", NullValueHandling = NullValueHandling.Ignore)]
+        public string LABEL_COLOR_2 { get; set; }
+    }
+}
