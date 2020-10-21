1ae1e93d71e81a0984f2abc1435c8a6902e46ee9
Sean Kennealy <spkennealy@gmail.com>
2020-10-21 16:59:00 -0700

    Models.Submittable.Labels added

---
+++ Models/Submittable/Labels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Labels
+    {
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string TYPE { get; set; }
+
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string URL { get; set; }
+
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public int? COUNT { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Label> ITEMS { get; set; }
+    }
+}
