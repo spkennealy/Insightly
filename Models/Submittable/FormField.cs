d2a1aace1c01a37ff5ef6e1fe6736e0eafc368f5
Sean Kennealy <spkennealy@gmail.com>
2020-10-21 16:57:48 -0700

    Models.Submittable.FormField added

---
+++ Models/Submittable/FormField.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class FormField
+    {
+        [JsonProperty("form_field_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? FORM_FIELD_ID { get; set; }
+
+        [JsonProperty("field_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string FIELD_TYPE { get; set; }
+
+        [JsonProperty("label", NullValueHandling = NullValueHandling.Ignore)]
+        public string LABEL { get; set; }
+
+        [JsonProperty("data", NullValueHandling = NullValueHandling.Ignore)]
+        public string DATA { get; set; }
+
+        [JsonProperty("blind", NullValueHandling = NullValueHandling.Ignore)]
+        public bool BLIND { get; set; }
+
+        [JsonProperty("order", NullValueHandling = NullValueHandling.Ignore)]
+        public int? ORDER { get; set; }
+    }
+}
