382030648cea77a6ac0254c36936c93994f04665
Sean Kennealy <spkennealy@gmail.com>
2020-10-22 09:09:43 -0700

    Models.Submittable.SubmittableWrapper added

---
+++ Models/Submittable/SubmittableWrapper.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class SubmittableWrapper
+    {
+        [JsonProperty("current_page", NullValueHandling = NullValueHandling.Ignore)]
+        public int? CURRENT_PAGE { get; set; }
+
+        [JsonProperty("total_pages", NullValueHandling = NullValueHandling.Ignore)]
+        public int? TOTAL_PAGES { get; set; }
+
+        [JsonProperty("total_items", NullValueHandling = NullValueHandling.Ignore)]
+        public int? TOTAL_ITEMS { get; set; }
+
+        [JsonProperty("items_per_page", NullValueHandling = NullValueHandling.Ignore)]
+        public int? ITEMS_PER_PAGE { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public object ITEMS { get; set; }
+    }
+}
