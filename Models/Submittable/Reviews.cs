84ddb259c86c28027d910116699cc5326fe4df4d
Sean Kennealy <spkennealy@gmail.com>
2020-10-22 09:06:45 -0700

    Models.Submittable.Reviews added

---
+++ Models/Submittable/Reviews.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Reviews
+    {
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public int? COUNT { get; set; }
+
+        [JsonProperty("score", NullValueHandling = NullValueHandling.Ignore)]
+        public int? SCORE { get; set; }
+
+        [JsonProperty("average", NullValueHandling = NullValueHandling.Ignore)]
+        public double? AVERAGE { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Review> ITEMS { get; set; }
+    }
+}
