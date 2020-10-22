200f87aa5e18b7d5921c6c17900eda1194b421fe
Sean Kennealy <spkennealy@gmail.com>
2020-10-22 09:08:48 -0700

    Models.Submittable.SubmittableFile added

---
+++ Models/Submittable/SubmittableFile.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class SubmittableFile
+    {
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ID { get; set; }
+
+        [JsonProperty("guid", NullValueHandling = NullValueHandling.Ignore)]
+        public string GUID { get; set; }
+
+        [JsonProperty("form_field_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? FORM_FIELD_ID { get; set; }
+
+        [JsonProperty("file_name", NullValueHandling = NullValueHandling.Ignore)]
+        public string FILE_NAME { get; set; }
+
+        [JsonProperty("file_extension", NullValueHandling = NullValueHandling.Ignore)]
+        public string FILE_EXTENSION { get; set; }
+
+        [JsonProperty("file_size", NullValueHandling = NullValueHandling.Ignore)]
+        public long? FILE_SIZE { get; set; }
+
+        [JsonProperty("mime_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string MIME_TYPE { get; set; }
+
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string URL { get; set; }
+
+        [JsonProperty("transcoded_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string TRANSCODED_URL { get; set; }
+    }
+}
