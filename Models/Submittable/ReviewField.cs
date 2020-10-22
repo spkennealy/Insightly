2b14f330c0b725eb6bb6392f0eccbce764b4dbf7
Sean Kennealy <spkennealy@gmail.com>
2020-10-22 09:06:04 -0700

    Models.Submittable.ReviewField added

---
+++ Models/Submittable/ReviewField.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class ReviewField
+    {
+        [JsonProperty("field_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string FIELD_TYPE { get; set; }
+
+        [JsonProperty("label", NullValueHandling = NullValueHandling.Ignore)]
+        public string LABEL { get; set; }
+
+        [JsonProperty("data", NullValueHandling = NullValueHandling.Ignore)]
+        public string DATA { get; set; }
+
+        [JsonProperty("scoring_value", NullValueHandling = NullValueHandling.Ignore)]
+        public int? SCORING_VALUE { get; set; }
+
+        [JsonProperty("order", NullValueHandling = NullValueHandling.Ignore)]
+        public int? ORDER { get; set; }
+    }
+}
