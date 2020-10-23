891417c0780cbaabafe3028f9d87af5a0650e1e6
Sean Kennealy <spkennealy@gmail.com>
2020-10-23 09:18:33 -0700

    Models.Submittable.Votes added

---
+++ Models/Submittable/Votes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Submittable
+{
+    public class Votes
+    {
+        [JsonProperty("count", NullValueHandling = NullValueHandling.Ignore)]
+        public int? COUNT { get; set; }
+
+        [JsonProperty("score", NullValueHandling = NullValueHandling.Ignore)]
+        public int? SCORE { get; set; }
+
+        [JsonProperty("average", NullValueHandling = NullValueHandling.Ignore)]
+        public double? AVERAGE { get; set; }
+
+        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> ITEMS { get; set; } // Don't know what the vote schema looks like
+    }
+}
