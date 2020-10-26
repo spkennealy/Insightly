7467e2bbe891f5b21b30aadb1372b462562b8888
Sean Kennealy <spkennealy@gmail.com>
2020-10-26 08:56:57 -0700

    Models.Freshdesk.Agent added

---
+++ Models/Freshdesk/Agent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Models.Freshdesk
+{
+    public class Agent : IFDObject
+    {
+        [JsonProperty("available", NullValueHandling = NullValueHandling.Ignore)]
+        public bool AVAILABLE { get; set; }
+
+        [JsonProperty("occasional", NullValueHandling = NullValueHandling.Ignore)]
+        public bool OCCASIONAL { get; set; }
+
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ID { get; set; }
+
+        [JsonProperty("ticket_scope", NullValueHandling = NullValueHandling.Ignore)]
+        public int? TICKET_SCOPE { get; set; }
+
+        [JsonProperty("signature", NullValueHandling = NullValueHandling.Ignore)]
+        public string SIGNATURE { get; set; }
+
+        [JsonProperty("group_ids", NullValueHandling = NullValueHandling.Ignore)]
+        public List<long?> GROUP_IDS { get; set; }
+
+        [JsonProperty("role_ids", NullValueHandling = NullValueHandling.Ignore)]
+        public List<long?> ROLE_IDS { get; set; }
+
+        [JsonProperty("skill_ids", NullValueHandling = NullValueHandling.Ignore)]
+        public List<long?> SKILL_IDS { get; set; }
+
+        [JsonProperty("created_at", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? CREATED_AT { get; set; }
+
+        [JsonProperty("updated_at", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? UPDATED_AT { get; set; }
+
+        [JsonProperty("last_active_at", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? LAST_ACTIVE_AT { get; set; }
+
+        [JsonProperty("available_since", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? AVAILABLE_SINCE { get; set; }
+
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string TYPE { get; set; }
+
+        [JsonProperty("contact", NullValueHandling = NullValueHandling.Ignore)]
+        public FDContact CONTACT { get; set; }
+
+        [JsonProperty("custom_fields", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, object> CUSTOM_FIELDS { get; set; }
+    }
+}
