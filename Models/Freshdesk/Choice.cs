45b1d18485add13237e92ad1830ba15c7fcafda5
Sean Kennealy <spkennealy@gmail.com>
2020-10-26 08:59:01 -0700

    Models.Freshdesk.Choice added

---
+++ Models/Freshdesk/Choice.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Freshdesk
+{
+    public class Choice
+    {
+        [JsonProperty("id")]
+        public int? ID { get; set; }
+
+        [JsonProperty("label_for_customers")]
+        public string LABEL_FOR_CUSTOMERS { get; set; }
+
+        [JsonProperty("value")]
+        public string VALUE { get; set; }
+
+        [JsonProperty("stop_sla_timer")]
+        public bool STOP_SLA_TIMER { get; set; }
+
+        [JsonProperty("default")]
+        public bool DEFAULT { get; set; }
+
+        [JsonProperty("position")]
+        public int? POSITION { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool DELETED { get; set; }
+    }
+}
