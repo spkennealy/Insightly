d09d22811bc00bf62834a841a795be089c814338
Sean Kennealy <spkennealy@gmail.com>
2020-10-27 08:58:03 -0700

    Models.Freshdesk.CompanySearchResult added

---
+++ Models/Freshdesk/CompanySearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Models.Freshdesk
+{
+    public class CompanySearchResult
+    {
+        [JsonProperty("companies", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Company> COMPANIES { get; set; }
+    }
+}
