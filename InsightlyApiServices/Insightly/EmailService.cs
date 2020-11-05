f95c1b0700d9cf582b0a454abeefac944b6a0785
Sean Kennealy <spkennealy@gmail.com>
2020-11-05 08:50:25 -0800

    Services.Insightly.EmailService skeleton added

---
+++ InsightlyApiServices/Insightly/EmailService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Services.Insightly
+{
+    public class EmailService //: BaseService<Email>
+    {
+        public EmailService(string apiKey) : base(apiKey, "Emails")
+        {
+        }
+    }
+}
