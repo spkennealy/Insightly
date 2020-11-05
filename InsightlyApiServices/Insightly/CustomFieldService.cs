75c5c60937e43ed89f5b02aa06407f7ccaf6d0c6
Sean Kennealy <spkennealy@gmail.com>
2020-11-05 08:47:43 -0800

    Services.Insightly.CustomFieldService added

---
+++ InsightlyApiServices/Insightly/CustomFieldService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Models.Insightly.CustomFieldDetails;
+
+namespace Services.Insightly
+{
+    public class CustomFieldService : BaseService<CustomFieldDetail>
+    {
+        public CustomFieldService(string apiKey, string baseObj) : base(apiKey, $"CustomFields/{baseObj}")
+        {
+        }
+
+        public async Task<CustomFieldDetail> SearchByFieldName(string field)
+        {
+            return await GetByUrlAsync<CustomFieldDetail>($"Search?fieldName={field}");
+        }
+    }
+}
