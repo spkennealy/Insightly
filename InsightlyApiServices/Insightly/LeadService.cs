acdf13d70df56b64cb425f3e428802973f70e21a
Sean Kennealy <spkennealy@gmail.com>
2020-11-09 09:57:50 -0800

    Services.Insightly.LeadSourceService added

--- InsightlyApiServices/Insightly/LeadService.cs
+++ InsightlyApiServices/Insightly/LeadService.cs
@@ -1,10 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Tools;
+using Models.Insightly;
+
 namespace Services.Insightly
 {
-    public class LeadService
+    public class LeadService : CrudService<Lead>
     {
-        public LeadService()
+        public LeadService(string apiKey) : base(apiKey, "Leads")
         {
+        }
+
+        public IEnumerable<Lead> GetAllLeads()
+        {
+            try
+            {
+                Utils.LogMessage($"Getting all leads...");
+
+                var leads = GetAll().Result;
+                var s = leads != null && leads.Count() > 1 ? "s" : string.Empty;
+                if (leads != null && leads.Count() > 0) Utils.LogMessage($"Successfully found {leads.Count()} lead{s}!");
+                return leads;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogError($"Getting all leads: {ex.Message}");
+                throw;
+            }
+        }
+
+        public Lead GetLead(long? leadId)
+        {
+            try
+            {
+                if (leadId == null) return null;
+                Utils.LogMessage($"Getting lead #{leadId}...");
+
+                var lead = Get((long)leadId).Result;
+                if (lead != null) Utils.LogMessage($"Successfully retrieved lead, {lead.FIRST_NAME} {lead.LAST_NAME}!");
+                return lead;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogError($"Getting lead #{leadId}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public IEnumerable<Lead> SearchLeadsByFieldValue(string fieldName, object fieldValue)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(fieldName) || fieldValue == null) return null;
+                Utils.LogMessage($"Searching for leads with {fieldValue} in {fieldName}...");
+
+                var leads = Search(fieldName, fieldValue).Result;
+                var s = leads != null && leads.Count() > 1 ? "s" : string.Empty;
+                if (leads != null && leads.Count() > 0) Utils.LogMessage($"Successfully found {leads.Count()} lead{s}!");
+                return leads;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogError($"Searching for leads with {fieldValue} in {fieldName}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public Lead CreateLead(Lead lead)
+        {
+            try
+            {
+                if (lead == null) return null;
+                Utils.LogMessage($"Creating lead {lead?.FIRST_NAME} {lead?.LAST_NAME}...");
+
+                lead = Create(lead)?.Result;
+                if (lead != null) Utils.LogMessage($"Successfully created lead #{lead.LEAD_ID}!");
+                return lead;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogMessage($"Error creating lead {lead?.FIRST_NAME} {lead?.LAST_NAME}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public Lead UpdateLead(Lead lead)
+        {
+            try
+            {
+                if (lead == null) return null;
+                Utils.LogMessage($"Updating lead {lead?.FIRST_NAME} {lead?.LAST_NAME}...");
+
+                lead = Update(lead)?.Result;
+                if (lead != null) Utils.LogMessage($"Successfully updated lead #{lead.LEAD_ID}!");
+                return lead;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogMessage($"Error updating lead {lead?.FIRST_NAME} {lead?.LAST_NAME}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public bool DeleteLead(long? leadId)
+        {
+            try
+            {
+                if (leadId == null) return false;
+                Utils.LogMessage($"Deleting lead #{leadId}...");
+
+                var deleted = Delete((long)leadId).Result;
+                if (deleted) Utils.LogMessage($"Successfully deleted lead #{leadId}!");
+                return deleted;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogError($"Deleting lead #{leadId}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public static Lead CopyLead(Lead lead)
+        {
+            return new Lead() { LEAD_ID = lead.LEAD_ID, CUSTOMFIELDS = new List<CustomField>() };
         }
     }
 }
---
+++ InsightlyApiServices/Insightly/LeadSourceService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Tools;
+using Models.Insightly;
+
+namespace Services.Insightly
+{
+    public class LeadSourceService : BaseService<LeadSource>
+    {
+        public LeadSourceService(string apiKey) : base(apiKey, "LeadSources")
+        {
+        }
+
+        public IEnumerable<LeadSource> GetLeadSources()
+        {
+            try
+            {
+                Utils.LogMessage($"Getting a list of lead sources...");
+
+                var leadSources = GetByUrlAsync<IEnumerable<LeadSource>>("")?.Result;
+                if (leadSources != null && leadSources.Count() > 0) Utils.LogMessage($"Successfully retrieved {leadSources.Count()} lead sources!");
+                return leadSources;
+            }
+            catch (Exception ex)
+            {
+                Utils.LogError($"Getting lead sources: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
