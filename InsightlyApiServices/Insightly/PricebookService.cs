4d7c874200e74a526027b5f90582240830c94771
Sean Kennealy <spkennealy@gmail.com>
2020-11-12 09:26:08 -0800

    Services.Insightly.PricebookService added

---
+++ InsightlyApiServices/Insightly/PricebookService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models.Insightly;
+
+namespace Services.Insightly
+{
+    public class PricebookService : CrudService<Pricebook>
+    {
+        public PricebookService(string apiKey) : base(apiKey, "Pricebook")
+        {
+        }
+
+        public async Task<IEnumerable<PricebookEntry>> GetPricebookEntries(long pricebookId)
+        {
+            return await GetByUrlAsync<IEnumerable<PricebookEntry>>($"{pricebookId}/PricebookEntry");
+        }
+    }
+}
