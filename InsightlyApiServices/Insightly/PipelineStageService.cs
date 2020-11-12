cd6da433ca8c17f321abbeaa706aa712dcf2a819
Sean Kennealy <spkennealy@gmail.com>
2020-11-12 09:06:31 -0800

    Services.Insightly.PipelineStageService added

---
+++ InsightlyApiServices/Insightly/PipelineStageService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Models.Insightly;
+
+namespace Services.Insightly
+{
+    public class PipelineStageService : BaseService<PipelineStage>
+    {
+        public PipelineStageService(string apiKey) : base(apiKey, "PipelineStages")
+        {
+        }
+
+        public async Task<PipelineStage> Get(long id)
+        {
+            return await apiClient.GetAsync<PipelineStage>($"{objectUrl}/{id}");
+        }
+    }
+}
