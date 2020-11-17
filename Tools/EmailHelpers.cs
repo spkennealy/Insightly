ac0d3afa23a3ec8c3a588e375983fe16972871aa
Sean Kennealy <spkennealy@gmail.com>
2020-11-17 08:37:32 -0800

    Tools.EmailHelpers added

---
+++ Tools/EmailHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Tools
+{
+    public static class EmailHelpers
+    {
+        public static string ParseEmailDomain(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return null;
+            return email?.Split('@')?.Last();
+        }
+    }
+}
