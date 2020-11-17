1ba87247198de512942f1ed8c764c3550d508d53
Sean Kennealy <spkennealy@gmail.com>
2020-11-17 08:33:04 -0800

    Tools.StringHelpers added

---
+++ Tools/StringHelpers.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Tools
+{
+    public static class StringHelpers
+    {
+        public static string RemoveHtmlTags(this string htmlString)
+        {
+            return Regex.Replace(htmlString, @"<(.|\n)*?>", string.Empty);
+        }
+
+        public static string ReSizeForMultiLineText(this string text)
+        {
+            if (text.Length > 4000)
+            {
+                text = text.Substring(0, 4000);
+            }
+            return text;
+        }
+
+        public static string ReplaceSpacesWithUnderscores(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return text;
+            return text.Replace(' ', '_');
+        }
+
+        public static string CapitalizeWord(this string word)
+        {
+            if (string.IsNullOrWhiteSpace(word)) return word;
+            if (word.Length == 1)
+            {
+                return word.ToUpper();
+            }
+            else
+            {
+                return word[0].ToString().ToUpper() + word.Substring(1, word.Length - 1);
+            }
+        }
+
+        public static string CapitalizeAllWords(this string sentence)
+        {
+            if (string.IsNullOrWhiteSpace(sentence)) return sentence;
+
+            string result = string.Empty;
+            string[] words = sentence.Split(' ');
+            foreach (var word in words)
+            {
+                result += CapitalizeWord(word);
+            }
+
+            return result;
+        }
+
+        public static string ConvertToSafeFileName(this string fileName)
+        {
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_');
+            }
+
+            return fileName.ReplaceSpacesWithUnderscores();
+        }
+    }
+}
