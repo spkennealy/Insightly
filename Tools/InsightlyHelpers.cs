5e9f89a76f2d459e5a503f0efe8381376166df26
Sean Kennealy <spkennealy@gmail.com>
2021-03-02 17:46:25 -0800

    InsightlyHelpers.CreateCustomField added

---
+++ Tools/InsightlyHelpers.cs
@@ -0,0 +1,17 @@
+using Common.Models.Insightly;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Tools
+{
+    public static class InsightlyHelpers
+    {
+        public static CustomField CreateCustomField(this InsightlyCustomFieldObject obj, string fieldName, object fieldValue)
+        {
+            if (obj.CUSTOMFIELDS == null) obj.CUSTOMFIELDS = new List<CustomField>();
+            var customField = new CustomField() { FIELD_NAME = fieldName, FIELD_VALUE = fieldValue };
+            obj.CUSTOMFIELDS.Add(customField);
+            return customField;
+        }
+    }
+}
