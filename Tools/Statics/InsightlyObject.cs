ef313df18a4a0339a0446f1a400af7622ea17687
Sean Kennealy <spkennealy@gmail.com>
2021-03-23 08:58:44 -0700

    Added Tools.Statics.InsightlyObject

---
+++ Tools/Statics/InsightlyObject.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools.Statics
+{
+    public static class InsightlyObject
+    {
+        public const string TASKS = "Tasks";
+        public const string LEAD = "Lead";
+        public const string LEADS = "Leads";
+        public const string CONTACT = "Contact";
+        public const string CONTACTS = "Contacts";
+        public const string ORGANISATION = "Organisation";
+        public const string ORGANISATIONS = "Organisations";
+        public const string OPPORTUNITY = "Opportunity";
+        public const string OPPORTUNITIES = "Opportunities";
+        public const string PROJECT = "Project";
+        public const string PROJECTS = "Projects";
+        public const string EVENT = "Event";
+        public const string EVENTS = "Events";
+        public const string PIPELINES = "Pipelines";
+        public const string NOTES = "Notes";
+    }
+}
