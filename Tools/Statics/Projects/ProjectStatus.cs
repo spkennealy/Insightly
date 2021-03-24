b43f588f4523a9b6769d16eee49c371641faabaa
Sean Kennealy <spkennealy@gmail.com>
2021-03-24 08:57:54 -0700

    Added Tools.Statics.Project.ProjectStatus

---
+++ Tools/Statics/Projects/ProjectStatus.cs
@@ -0,0 +1,11 @@
+namespace Tools.Statics.Projects
+{
+    public static class ProjectStatus
+    {
+        public const string NOT_STARTED = "NOT STARTED";
+        public const string IN_PROGRESS = "IN PROGRESS";
+        public const string DEFERRED = "DEFERRED";
+        public const string CANCELLED = "CANCELLED";
+        public const string COMPLETED = "COMPLETED";
+    }
+}
